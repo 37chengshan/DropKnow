f2ea2732a2adeb66473c9f5ec9199ae6c1716371
37chengshan <37chengshan@users.noreply.github.com>
2026-04-19 00:37:05 +0800

    Initial Commit

---
+++ DropKnow/DropKnowApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropKnowApp.swift
+//  DropKnow
+//
+//  Created by YuFei Chi on 2026/4/19.
+//
+
+import SwiftUI
+
+@main
+struct DropKnowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
